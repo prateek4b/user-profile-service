931de5e532d90688342c9d0e0a8e4a3e941447a2
prateek4b <shivcharanag293@gmail.com>
2021-06-27 09:24:15 +0530

    commit

--- src/main/java/com/guide/cm/userprofileservice/UserProfileServiceApplication.java
+++ src/main/java/com/guide/cm/userprofileservice/UserProfileServiceApplication.java
@@ -2,9 +2,12 @@ package com.guide.cm.userprofileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserProfileServiceApplication {
 
 	public static void main(String[] args) {
