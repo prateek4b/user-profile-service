2ade0b3edd47b4aa432e8b32fa4534ed82ad5551
prateek4b <shivcharanag293@gmail.com>
2021-07-05 17:24:17 +0530

    commit

--- src/main/java/com/guide/cm/userprofileservice/controller/HelloController.java
+++ src/main/java/com/guide/cm/userprofileservice/controller/HelloController.java
@@ -1,10 +1,17 @@
 package com.guide.cm.userprofileservice.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.servlet.support.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
+
+
 
     @GetMapping("/sayHello")
     public String sayHello() {
@@ -12,9 +19,40 @@ public class HelloController {
 
     }
 
+    @GetMapping("/showRequest")
+    public String showRequest(HttpServletRequest httpRequest) {
+       return httpRequest.getHeader("gateway")+"-->"+httpRequest.getParameter("gateway");
+    }
+
     @GetMapping("/sayRedirected")
     public String sayHelloRedirected() {
         return "HelloRedirected";
+
+    }
+
+
+    @GetMapping("/afterTime")
+    public String sayAfterTime() {
+        return "AfterTime";
+
+    }
+
+    @GetMapping("/time/afterTime")
+    public String sayPrefixAfterTime() {
+        return "PrefixAfterTime";
+
+    }
+
+    @GetMapping("/redirectThis")
+    public String sayRedirectThis() {
+        return "redirectThis";
+
+    }
+
+    @GetMapping("/retryer")
+    public String retry() {
+        System.out.println("Retried");
+        return "redirectThis";
 
     }
 
